2bec9f37350bd73ce26519296d2c83da36869f40
douglas aubre <boseallen192@gmail.com>
2025-11-07 19:46:51 +0530

    init commit

---
+++ src/main/java/com/forge/Hiyori/HiyoriApplication.java
@@ -0,0 +1,13 @@
+package com.forge.Hiyori;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiyoriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiyoriApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/forge/Hiyori/HiyoriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forge.Hiyori;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiyoriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
